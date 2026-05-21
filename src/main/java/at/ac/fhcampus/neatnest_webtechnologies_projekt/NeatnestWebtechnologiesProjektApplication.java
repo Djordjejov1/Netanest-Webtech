d0d365e259666e8723a0e353f51f351fa1429250
Djordjejov1 <djordje.00349@gmail.com>
2026-05-21 21:05:19 +0200

    Initial project setup

---
+++ src/main/java/at/ac/fhcampus/neatnest_webtechnologies_projekt/NeatnestWebtechnologiesProjektApplication.java
@@ -0,0 +1,13 @@
+package at.ac.fhcampus.neatnest_webtechnologies_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeatnestWebtechnologiesProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeatnestWebtechnologiesProjektApplication.class, args);
+    }
+
+}
